d57e86ee212d4fa5ebd0f6cadfc5ef9413ff7622
Zdenek Korvas <korvasz@seznam.cz>
2023-09-15 15:47:34 +0200

    added better handling input

--- geometry_game.py
+++ geometry_game.py
@@ -4,6 +4,9 @@ import turtle
 
 
 class Point:
+    """
+    class made to take point x,y
+    """
 
     def __init__(self, x, y):
         self.x = x
@@ -15,7 +18,7 @@ class Point:
         :param rectangle: random generated rectangle object.
         :return: Bool if guessed point is in rectangle or not.
         """
-
+        # checking if user input is between the rectangle points
         if (rectangle.coordinate1.x < self.x < rectangle.coordinate2.x and
                 rectangle.coordinate1.y < self.y < rectangle.coordinate2.y):
             return True
@@ -36,6 +39,9 @@ class Point:
 
 
 class Rectangle:
+    """
+    class creating rectangle, need 2 arguments
+    """
 
     def __init__(self, coordinate1, coordinate2):
         self.coordinate1 = coordinate1
@@ -50,27 +56,53 @@ class Rectangle:
         b = self.coordinate1.y - self.coordinate2.y
         t = turtle.Turtle()
         t.forward(a)  # a = length of first side
-        t.left(90)   # Turn turtle by 90 degree
+        t.right(90)   # Turn turtle by 90 degree
         t.forward(b)  # b length of second side
-        t.left(90)
+        t.right(90)
         t.forward(a)
-        t.left(90)
+        t.right(90)
         t.forward(b)
         return t
 
 
+def check_input(prompt):
+    """
+    function checking users input
+    :param prompt: prompt sentece
+    :return: valid input (integer)
+    """
+    while True:
+        user_input = input(prompt)
+        try:
+            user_input = int(user_input)
+            return user_input
+        except ValueError:
+            print('Wrong value, you must type an integer. Try again.')
+
+
+def random_cord() -> int:
+    """
+    generating random numbers
+    :return: random integer
+    """
+    return random.randint(0, 9)
+
+
 def main():
     # make random rectangle
-    rectangle = Rectangle(Point(random.randint(0, 9), random.randint(0, 9)),
-                          Point(random.randint(10, 19), random.randint(10, 19)))
+    rectangle = Rectangle(Point(random_cord(), random_cord()),
+                          Point((random_cord() + 10), (random_cord()+10)))
     # for testing purposes I will print coordinates of rectangle
     print(f'X:{rectangle.coordinate1.x}, Y:{rectangle.coordinate1.y} - '
           f'X:{rectangle.coordinate2.x},Y{rectangle.coordinate2.y}')
 
-    user_x = int(input('Guess x coordinate: '))
-    user_y = int(input('Guess y coordinate: '))
+    user_x = check_input('What is X coord: ')
+    user_y = check_input('What is Y coord: ')
+
     user_point = Point(user_x, user_y)
+    # print out if user was lucky and hit the spot
     print(user_point.in_rectangle(rectangle))
+    # printing GUI
     print(rectangle.turtle_drawing(), user_point.point_drawing())
     turtle.exitonclick()
 
