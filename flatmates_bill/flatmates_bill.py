5360fc9a3f33027cd279f5f70a84f6d39c997d1e
Zdeněk K <132506987+Zncrw@users.noreply.github.com>
2023-12-05 16:46:51 +0100

    Update flatmates_bill.py
    add generate method in class PdfReport

--- flatmates_bill/flatmates_bill.py
+++ flatmates_bill/flatmates_bill.py
@@ -1,3 +1,4 @@
+from fpdf import FPDF
 class Bill:
     """
     Object that has data about a bill(amount,period)
@@ -34,12 +35,28 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+        # Insert text
+        pdf.set_font(family='Times', style='B', size=24)
+        # Title
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', align='C', border=1, ln=1)
+        # text in cells
+        pdf.cell(w=100, h=40, txt='Period:', border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        # Insert name of 1st user and amount to pay
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill=bill, flatmate2=flatmate2)), border=1, ln=1)
+
+        pdf.output(name=self.filename)
 
 
-the_bill = Bill(amount=120, period='March 2021')
+the_bill = Bill(amount=120, period='July 2023')
 john = Flatmate(name='John', days_in_house=20)
 marry = Flatmate(name='Marry', days_in_house=25)
 
 print('john has to pay', john.pays(bill=the_bill, flatmate2=marry))
 print('marry has to pay', marry.pays(bill=the_bill, flatmate2=john))
+pdf_report = PdfReport(filename='report1.pdf')
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
