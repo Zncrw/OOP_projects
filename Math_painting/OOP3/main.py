b828186c5677665a772cf066614c30ef5f54c615
Zdenek Korvas <korvasz@seznam.cz>
2023-12-08 11:25:45 +0100

    added class and methods description

--- Math_painting/OOP3/main.py
+++ Math_painting/OOP3/main.py
@@ -1,13 +1,11 @@
 import numpy as np
 from PIL import Image
-#
-#
-# matrix = np.zeros((5, 4, 3), dtype=np.uint8)
-# matrix[:] = [255, 255, 0]
-#
-# img = Image.fromarray(matrix, 'RGB')
-# img.save('test.png')
+
+
 class Square:
+    """
+    class that will draw a square based on users input
+    """
 
     def __init__(self, x, y, side, color):
         self.color = color
@@ -16,10 +14,18 @@ class Square:
         self.x = x
 
     def draw(self, canvas):
-
+        """
+        method, that will draw a square on base canvas
+        :param canvas: user defined canvas
+        :return: square drawing
+        """
         pass
 
+
 class Rectangle:
+    """
+    class that will draw a rectangle based on user input.
+    """
 
     def __init__(self, x, y, width, height, color):
         self.color = color
@@ -29,26 +35,30 @@ class Rectangle:
         self.x = x
 
     def draw(self, canvas):
+        """
+        method that will draw rectangle on canvas
+        """
         pass
-
 
         
 class Canvas:
-
+    """
+    class that will make a canvas after getting user input
+    """
     def __init__(self, width, height, color):
         self.color = color
         self.height = height
         self.width = width
+
         # create empty 3D grid
+        self.matrix = np.zeros((5, 4, 3), dtype=np.uint8)
 
         # change 0,0,0 to users options
-        
-
-
+        self.matrix[:] = self.color
 
     def make(self, imagepath):
-        pass
-
-
-
-
+        """
+        method that will save final img
+        """
+        img = Image.fromarray(self.matrix, 'RGB')
+        img.save(f'{imagepath}.png')
