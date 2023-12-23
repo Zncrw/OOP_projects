ed808cc6b209e9e20998c4663094685932e2cfb6
Zdeněk K <132506987+Zncrw@users.noreply.github.com>
2023-12-23 09:12:13 +0100

    Update main.py
    complete the back-end of the app, add error handling

--- camera_photo_sharer/main.py
+++ camera_photo_sharer/main.py
@@ -2,38 +2,87 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 import time
+from filestack import Client
+from kivy.core.clipboard import Clipboard
+import webbrowser
 
 Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
     def start(self):
+        """
+        starts camera and change the text of button
+        """
         self.ids.camera.play = True
         # changing button name when camera is running
         self.ids.camera_button.text = 'Stop Camera'
+
         # setting background of camera widget
         self.ids.camera.texture = self.ids.camera._texture
 
     def stop(self):
+        """
+        stops the camera and change the text of the button
+        """
         self.ids.camera.play = False
+
         # changing button name when camera is running
         self.ids.camera_button.text = 'Start Camera'
         self.ids.camera.texture = None
 
     def capture(self):
+        """
+        capture the image of camera and saves that under the current time
+        as a file.
+        """
         # getting current time for the name of file
         current_time = time.strftime('S%M%H%-d%m%Y')
+
         # saving captured screen as file with current_time.png
-        filename = f'saved_imgs/{current_time}.png'
-        self.ids.camera.export_to_png(filename)
+        self.filename = f'saved_imgs/{current_time}.png'
+        self.ids.camera.export_to_png(self.filename)
+
         # changing screen to current captured img
         self.manager.current = 'image_screen'
-        self.manager.current_screen.ids.img.source = filename
+        self.manager.current_screen.ids.img.source = self.filename
 
 
 class ImageScreen(Screen):
-    pass
+    error_message = 'Create link first please!'
+    def create_link(self):
+        """
+        Acces the saved image file and uploads it to web and inserts the link
+        to Label widget.
+        """
+        # gets the current file path
+        file_path = App.get_running_app().root.ids.camera_screen.self.filename
 
+        # initialize the FileSharer class
+        fileshare = FileSharer(filepath=file_path)
+        self.url = fileshare.share()
+
+        # gets a current link under the Label widget
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+
+        """
+        method that copy the shareable link after pressing copy link button
+        """
+        try:
+            Clipboard.copy(self.url)
+        except AttributeError:
+            self.ids.link.text = self.error_message
+
+    def open_link(self):
+        """
+        method that will open a browser window with saved image
+        """
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.error_message
 
 class FileSharer:
     def __init__(self, filepath, api_key='8ba98d13-ed91-47fc-a3ce-a4f1b0a28679'):
@@ -41,6 +90,9 @@ class FileSharer:
         self.api_key = api_key
 
     def share(self):
+        """
+        method that will create a link using users API.key
+        """
         client = Client(self.api_key)
         new_file_link = client.upload(filepath=self.filepath)
         return new_file_link.url
